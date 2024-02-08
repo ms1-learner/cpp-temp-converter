2c192e152c1bb6fc737de6c0faa79050340a23b4
kharada2 <kota.harada@woven-planet.global>
2024-02-08 12:06:24 +0900

    add convertFahrenheitToCelsius function

--- temp-converter.cpp
+++ temp-converter.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
 void convertCelsiusToFahrenheit() {
-    std::cout << "Enter the temperature in Celsius: ";
-    double celsius;
-    std::cin >> celsius;
+  std::cout << "Enter the temperature in Celsius: ";
+  double celsius;
+  std::cin >> celsius;
 
-    double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
-    
-    std::cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit." << std::endl;
+  double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+  std::cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit." << std::endl;
 }
 
-int main() {
-    return 0;
+void convertFahrenheitToCelsius() {
+  std::cout << "Enter the temperature in Fahrenheit: ";
+  double fahrenheit;
+  std::cin >> fahrenheit;
+
+  double celsius = 5.0 / 9.0 * (fahrenheit - 32.0);
+
+  std::cout << fahrenheit << " degrees Fahrenheit is " << celsius << " degrees Celsius." << std::endl;
 }
+
+int main() { return 0; }

f621fd680fe2d20e8f676ded58ba02cd9b7d7c13
kharada2 <kota.harada@woven-planet.global>
2024-02-08 12:48:15 +0900

    add new func

--- temp-converter.cpp
+++ temp-converter.cpp
@@ -20,17 +20,43 @@ void convertFahrenheitToCelsius() {
   std::cout << fahrenheit << " degrees Fahrenheit is " << celsius << " degrees Celsius." << std::endl;
 }
 
+void convertKelvinToCelsius() {
+  std::cout << "Enter the temperature in Kelvin: ";
+  double kelvin;
+  std::cin >> kelvin;
+
+  double celsius = kelvin - 273.15;
+
+  std::cout << kelvin << " degrees Fahrenheit is " << celsius << " degrees Celsius." << std::endl;
+}
+
+void convertKelvinToFahrenheit() {
+  std::cout << "Enter the temperature in Kelvin: ";
+  double kelvin;
+  std::cin >> kelvin;
+
+  double celsius = kelvin - 273.15;
+  double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+  std::cout << kelvin << " degrees Fahrenheit is " << fahrenheit << " degrees Fahrenheit." << std::endl;
+}
+
 int main() {
   std::cout << "Enter c if you want to convert from Fahrenheit to Celsius" << std::endl;
   std::cout << "Enter f if you want to convert from Celsius to Fahrenheit" << std::endl;
-  char input;
-  std::cin >> input;
-  if (input == 'c') {
-    convertCelsiusToFahrenheit();
-  } else if (input == 'f') {
-    convertFahrenheitToCelsius();
-  } else {
-    std::cout << "Incorrect input. Please try again later" << std::endl;
+
+  while (true) {
+    char input;
+    std::cin >> input;
+    if (input == 'c') {
+      convertFahrenheitToCelsius();
+      break;
+    } else if (input == 'f') {
+      convertCelsiusToFahrenheit();
+      break;
+    } else {
+      std::cout << "Incorrect input. Please try again later" << std::endl;
+    }
   }
 
   return 0;

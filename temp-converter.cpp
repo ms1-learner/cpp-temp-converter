16aeca938e5a3a9ab74f4a124aff8494263477a1
Yuma Sumi <yuma.sumi@woven-planet.global>
2024-02-09 13:28:03 +0900

    add converter

--- temp-converter.cpp
+++ temp-converter.cpp
@@ -10,6 +10,16 @@ void convertCelsiusToFahrenheit() {
     std::cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit." << std::endl;
 }
 
+void convertFahrenheitToCelsius() {
+    std::cout << "Enter the temperature in Fahreinheit: ";
+    double fahreinheit;
+    std::cin >> fahreinheit;
+
+    double celsius = 5.0 / 9.0 * (fahreinheit - 32.0);
+    
+    std::cout << fahreinheit << " degrees Fahrenheit is " << celsius << " degrees Celsius." << std::endl;
+}
+
 int main() {
     return 0;
 }
